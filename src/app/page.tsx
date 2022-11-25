cfd6fdbf33cd1d3a52c3cc01f581529ea1454470
Lautaro <lautaroef@gmail.com>
2022-11-25 17:11:55 -0300

    Comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,21 +13,15 @@ function Home() {
 export default Home;
 
 /*
-Therapeutical Educational Center | https://sacra.vercel.app/
-Project description:
-• This is a project for a Therapeutical Educational Center. It is a website that shows the services that the center offers, the information, and the location of each center.
-• Integrated Google Maps API and Leaflet library in order to display interactive and highly-responsive geo maps.
-• Implemented Cloudinary services to handle the upload and retrieval of each institution's images.
-• Also used Yup library for input validation.
-• Hosted the Backend using Supabase database management system.
-• Applied software development using React.js, JavaScript, Next.js, Redux, Node.js, Express, PostgreSQL, and Git.
-
+I think there is a bug in the job history tab, as every time I want to remove an image from one of the projects, all the images are removed. Then I try to upload the photos again and I realize the images are being added one after the other, no deletion happened, so now I have 16 duplicate photos when I only want to show 4
+Unable to delete project's images on the "Work History" tab. Now I have 16 duplicate photos when I only want to show 4.
 */
 
 /*
 Achieved a performance score of 96 out of 100 and an SEO score of 100/100 according to Google Lighthouse and Vercel engine.
 Achieved a very easy-to-customize template where the client can change the colors, fonts, images, and text with just a few clicks.
 Achieved an SEO-friendly website that has a lot of features that will help the client rank higher on Google.
+Out-of-the-box SEO and social media marketing make your website ready to go for whatever you want to do.
 Provided a 100% responsive product with a proper code structure that is scalable and maintainable in case the customer wants new features.
 Used the latest technologies and tools to achieve the best performance and SEO.
 Delivered a fully responsive template that works on all devices.
